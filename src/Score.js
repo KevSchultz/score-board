e390421d264cc9996c0129e236d7bb67c9600c5e
Kevin Schultz <ksschult@ucsc.edu>
2023-12-15 19:03:23 -0800

    depolying with github pages

--- src/Score.js
+++ src/Score.js
@@ -3,7 +3,10 @@ import Typography from '@mui/material/Typography';
 import { Button, IconButton } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+import ChristmasPingPong from './christmaspingpong.png';
+import Box from '@mui/material/Box';
 import Flip from "./Flip";
+import Card from '@mui/material/Card';
 import "./App.css";
 
 function limitScore(score) {
@@ -23,47 +26,58 @@ function Score() {
     useEffect(() => {
         const interval = setInterval(() => {
             setPlayerTwoScore(playerTwoScore => playerTwoScore > 0 ? playerTwoScore - 1 : 0);
-        }, 1000);
+        }, 10000);
         return () => clearInterval(interval);
     });
 
     return (
         <>
-            <div className="scores" style={{marginTop: '100px'}}>
-                <div>
-                    <Flip value={playerOneScore.toString().padStart(2, '0')} />
-                    <IconButton
-                        style={{ color: '#ff0000' }}
-                        variant="contained" onClick={e => setPlayerOneScore(limitScore(playerOneScore - 1))}>
-                        <RemoveIcon style={{ fontSize: '100px' }} />
-                    </IconButton>
-                    <IconButton
-                        style={{ color: '#ff0000' }}
-                        variant="contained" onClick={e => setPlayerOneScore(limitScore(playerOneScore + 1))}>
-                        <AddIcon style={{ fontSize: '100px' }} />
-                    </IconButton>
-                    <Typography style={{ fontSize:'100px', color: "white", fontFamily: 'Great Vibes, cursive' }}>
-                        Kevin
-                    </Typography>
-                </div>
-                <div>
-                    <Flip value={playerTwoScore.toString().padStart(2, '0')} />
-                    <IconButton
-                        style={{ color: '#ff0000' }}
-                        variant="contained" onClick={e => setPlayerTwoScore(limitScore(playerTwoScore - 1))}>
-                        <RemoveIcon style={{ fontSize: '100px' }} />
-                    </IconButton>
-                    <IconButton
-                        style={{ color: '#ff0000' }}
-                        variant="contained" onClick={e => setPlayerTwoScore(limitScore(playerTwoScore + 1))}>
-                        <AddIcon style={{ fontSize: '100px' }} />
-                    </IconButton>
-                    <Typography style={{ fontSize:'100px', color: "white", fontFamily: 'Great Vibes, cursive' }}>
-                        Kaitlin
-                    </Typography>
+            <div className="scores" style={{ marginTop: '100px' }}>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <div>
+                        <div style={{ borderRight: '5px solid white' }}>
+                            <Flip value={playerOneScore.toString().padStart(2, '0')} />
+                        </div>
+                        <div>
+                            <IconButton
+                                style={{ color: '#ff0000' }}
+                                variant="contained" onClick={e => setPlayerOneScore(limitScore(playerOneScore - 1))}>
+                                <RemoveIcon style={{ fontSize: '100px' }} />
+                            </IconButton>
+                            <IconButton
+                                style={{ color: '#ff0000' }}
+                                variant="contained" onClick={e => setPlayerOneScore(limitScore(playerOneScore + 1))}>
+                                <AddIcon style={{ fontSize: '100px' }} />
+                            </IconButton>
+                        </div>
+                        <Typography style={{ fontSize: '100px', color: "white", fontFamily: 'Great Vibes, cursive' }}>
+                            Kevin
+                        </Typography>
+                    </div>
+                    <div>
+                        <div style={{ borderLeft: '5px solid white'}}>
+                            <Flip value={playerTwoScore.toString().padStart(2, '0')} />
+                        </div>
+                        <div>
+                            <IconButton
+                                style={{ color: '#ff0000' }}
+                                variant="contained" onClick={e => setPlayerTwoScore(limitScore(playerTwoScore - 1))}>
+                                <RemoveIcon style={{ fontSize: '100px' }} />
+                            </IconButton>
+                            <IconButton
+                                style={{ color: '#ff0000' }}
+                                variant="contained" onClick={e => setPlayerTwoScore(limitScore(playerTwoScore + 1))}>
+                                <AddIcon style={{ fontSize: '100px' }} />
+                            </IconButton>
+                        </div>
+                        <Typography style={{ fontSize: '100px', color: "white", fontFamily: 'Great Vibes, cursive' }}>
+                            Kaitlin
+                        </Typography>
+                    </div>
                 </div>
             </div>
-            <div className="buttons">
+            <div style={{ display: "flex", flexDirection: "row", justifyContent: 'center' }}>
+                <Box component="img" sx={{ height: 300, width: 300, transform: 'translateY(-300px)' }} src={ChristmasPingPong} />
             </div>
         </>
     );
